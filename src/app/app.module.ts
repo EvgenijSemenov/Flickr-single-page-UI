916fab15ebd128cdbb98b6587094929237fa17d2
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-07-27 18:49:38 +0300

    Add FlickrLoginService in app.module.ts provider section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FlickrLoginService } from './flickr-login.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [FlickrLoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
