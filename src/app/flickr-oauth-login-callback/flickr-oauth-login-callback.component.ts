0fe7a27b28b98e17897faf3322fe3d793261374c
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-07-29 22:17:37 +0300

    Create flickrOauthCallbeck method in FlickrOauthLoginCallbackComponent

--- src/app/flickr-oauth-login-callback/flickr-oauth-login-callback.component.ts
+++ src/app/flickr-oauth-login-callback/flickr-oauth-login-callback.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { FlickrRequestUrlBuilder } from '../flickr/flickr-request-url-builder';
 
 @Component({
   selector: 'app-flickr-oauth-login-callback',
-  templateUrl: './flickr-oauth-login-callback.component.html',
-  styleUrls: ['./flickr-oauth-login-callback.component.css']
 })
 export class FlickrOauthLoginCallbackComponent {
 
   constructor() {
+    this.flickrOauthCallbeck()
+  }
+
+  private flickrOauthCallbeck() {
     let urlParams: string = window.location.href.split("?")[1];
     let event = new CustomEvent("FlickrOauthCallbeck", {
       "detail": {
-        "oauth_token": this.paramValue(urlParams, "oauth_token"),
-        "oauth_verifier": this.paramValue(urlParams, "oauth_verifier")
+        "oauth_token": FlickrRequestUrlBuilder.urlParamValue(urlParams, "oauth_token"),
+        "oauth_verifier": FlickrRequestUrlBuilder.urlParamValue(urlParams, "oauth_verifier")
       }
     });
 
@@ -20,18 +23,4 @@ export class FlickrOauthLoginCallbackComponent {
     window.close();
   }
 
-  private paramValue(paramsString: string, paramName: string): string {
-    let value: string = "";
-    let params: string[] = paramsString.split("&");
-
-    for (let i = 0; i < params.length; i++) {
-      if (params[i].startsWith(paramName + "=")) {
-
-        value = params[i].split("=")[1];
-        break;
-      }
-    }
-
-    return value;
-  }
 }
