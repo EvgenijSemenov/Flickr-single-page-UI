093d7a35e98f8261bf02811a6dc01413139cf3dd
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-07-29 22:18:38 +0300

    Add RouterModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Renderer } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FlickrLoginService } from './flickr-login.service';
+import { FlickrOauthLoginCallbackComponent } from './flickr-oauth-login-callback/flickr-oauth-login-callback.component';
+import { FlickrLoginComponent } from './flickr-login/flickr-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlickrOauthLoginCallbackComponent,
+    FlickrLoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: FlickrLoginComponent
+      },
+      {
+        path: 'flickr-oauth-callback',
+        component: FlickrOauthLoginCallbackComponent
+      }
+    ])
   ],
   providers: [FlickrLoginService],
   bootstrap: [AppComponent]
