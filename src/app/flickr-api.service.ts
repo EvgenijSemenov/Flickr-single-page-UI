9bd58b2bd64cab02096cbd8da5dad46526be6bf9
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-08-01 07:49:46 +0300

    Add photosetsTotal in FlickrApiService

--- src/app/flickr-api.service.ts
+++ src/app/flickr-api.service.ts
@@ -4,19 +4,23 @@ import { Photoset } from './model/photoset';
 import { FlickrRequestUrlBuilder } from './flickr/flickr-request-url-builder';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class FlickrApiService {
 
   constructor(private http: Http) { }
 
+  public photosetsTotal(userId: string, appCrdential): Observable<number> {
     let extraParams: string = "user_id=" + userId;
-    extraParams += "&primary_photo_extras" + primaryPhotoExtras;
-    let url: string = FlickrRequestUrlBuilder.apiUrl("flickr.photosets.getList", appCrdential, extraParams);
+    extraParams += "&page=1";
+    extraParams += "&per_page=0";
+    let url: string = FlickrRequestUrlBuilder.apiUrl(appCrdential, "flickr.photosets.getList", extraParams);
     
     return this.http.get(url)
-      .map((res: Response) => res.text()["photoset"])
+      .map((res: Response) => res.json().photosets.total as number)
+      .catch((err:any) => Observable.throw(err.json().error || 'Server error'));
+  }
+
   public photosetListByUserId(userId: string, page: number, perPage: number, appCrdential): Observable<Photoset[]> {
     let extraParams: string = "user_id=" + userId;
     extraParams += "&primary_photo_extras=url_sq";
