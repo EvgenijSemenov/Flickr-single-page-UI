0a68f36c2be8a1130a035d0dd9dfcc9ec9028547
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-08-01 07:57:55 +0300

    Create userId, fullName, photosetList, photosetsTotal and photosetPerPage feild in AlbumsListComponent

--- src/app/albums-list/albums-list.component.ts
+++ src/app/albums-list/albums-list.component.ts
@@ -1,4 +1,10 @@
+import { Data } from '@angular/router';
+import { PrimaryPhotoExtras } from '../model/primary-photo-extras';
+import { Content } from '../model/content';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { FlickrApiService } from '../flickr-api.service';
+import { Photoset } from '../model/photoset';
 
 @Component({
   selector: 'app-albums-list',
@@ -8,6 +14,11 @@ import { Component, OnInit } from '@angular/core';
 export class AlbumsListComponent implements OnInit {
 
   constructor() { }
+  private userId: string = "";
+  private fullName: string = "";
+  private photosetList: Photoset[] = [];
+  private photosetsTotal: number = 0;
+  private photosetPerPage: number = 10;
 
   ngOnInit() {
   }
