02a29cd54af322849d9e848690249ac9d667d6ef
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-08-02 00:49:26 +0300

    Add description and dateuploaded fields

--- src/app/model/photo.ts
+++ src/app/model/photo.ts
@@ -1,10 +1,14 @@
+import { Content } from './content';
+
 export class Photo {
 
   constructor(
     public id: string,
     public title: string,
+    public description: Content,
     public url_sq: string,
     public dateupload: string,
+    public dateuploaded: string,
     public originalformat: string
   ){ }
 
