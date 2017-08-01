f6abe07f10c1e503e2172ea71358169dd56f7a7b
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-08-01 07:48:22 +0300

    Create photosetListByUserId method in FlickrApiService

--- src/app/flickr-api.service.ts
+++ src/app/flickr-api.service.ts
@@ -11,13 +11,21 @@ export class FlickrApiService {
 
   constructor(private http: Http) { }
 
-  public photosetListByUserId(userId: string, primaryPhotoExtras: string, appCrdential): Observable<Photoset[]> {
     let extraParams: string = "user_id=" + userId;
     extraParams += "&primary_photo_extras" + primaryPhotoExtras;
     let url: string = FlickrRequestUrlBuilder.apiUrl("flickr.photosets.getList", appCrdential, extraParams);
     
     return this.http.get(url)
       .map((res: Response) => res.text()["photoset"])
+  public photosetListByUserId(userId: string, page: number, perPage: number, appCrdential): Observable<Photoset[]> {
+    let extraParams: string = "user_id=" + userId;
+    extraParams += "&primary_photo_extras=url_sq";
+    extraParams += "&page=" + page;
+    extraParams += "&per_page=" + perPage;
+    let url: string = FlickrRequestUrlBuilder.apiUrl(appCrdential, "flickr.photosets.getList", extraParams);
+    
+    return this.http.get(url)
+      .map((res: Response) => res.json().photosets.photoset as Photoset[])
       .catch((err:any) => Observable.throw(err.json().error || 'Server error'));
   }
 
