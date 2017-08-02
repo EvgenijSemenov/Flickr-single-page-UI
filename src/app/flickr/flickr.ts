276685e196234c35da43e69702d0c4c2e835549f
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-08-02 11:32:03 +0300

    Create isAuthorized method

--- src/app/flickr/flickr.ts
+++ src/app/flickr/flickr.ts
@@ -2,6 +2,18 @@ import { environment } from '../../environments/environment';
 
 export class Flickr {
 
+  public static isAuthorized(): boolean {
+    let result: boolean = false;
+    if (localStorage.getItem("id") 
+          && localStorage.getItem("fullname")
+          && localStorage.getItem("oauth_token") 
+          && localStorage.getItem("oauth_token_secret")) {
+      result = true;
+    }
+
+    return result;
+  }
+
   public static saveAuthCredential(oauthToken: string, oauthTokenSecret: string) {
     localStorage.setItem("oauth_token", oauthToken);
     localStorage.setItem("oauth_token_secret", oauthTokenSecret);
